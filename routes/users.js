e63b3e932566da1fa128a4f427b0f6b3adb54aed
Alexey Omelchenko <alexey.omelchenko@gmail.com>
2016-10-17 21:44:27 +0000

    added html parser to get the biggest picture in infobox (portrait)

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,52 @@
-//http://stackoverflow.com/questions/9548074/nodejs-how-to-add-image-data-from-file-into-canvas
-
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var fs = require('fs');
+var htmlparser = require("htmlparser2");
 
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
  
  request.get('https://ru.wikipedia.org/wiki/%D0%91%D1%83%D0%BB%D0%B0%D0%BA-%D0%91%D0%B0%D0%BB%D0%B0%D1%85%D0%BE%D0%B2%D0%B8%D1%87,_%D0%A1%D1%82%D0%B0%D0%BD%D0%B8%D1%81%D0%BB%D0%B0%D0%B2_%D0%9D%D0%B8%D0%BA%D0%BE%D0%B4%D0%B8%D0%BC%D0%BE%D0%B2%D0%B8%D1%87', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        var imgSrc = body.substring(body.indexOf("infobox", 0));
-        imgSrc = imgSrc.substring(imgSrc.indexOf("src=", 0));
-        imgSrc = imgSrc.substring(5, imgSrc.indexOf("\"", 7));
+        var handler = new htmlparser.DomHandler(function (error, dom) {
+          if (error) {
+             //[...do something for errors...]
+             console.log("parse error");
+          }
+          else {
+            //[...parsing done, do something...]
+            var fail = false;
+            var domUtils = htmlparser.DomUtils;
+            
+            //найдём все img в таблице с классом infobox
+            dom = domUtils.getElements({ class: "infobox" }, dom, true);
+            dom = domUtils.getElementsByTagName("img", dom, true);
+            
+            if (dom.length == 0) {fail = true;}
+            else {
+              var maxWidth = 100; // иконки и значки имеют ширину менее 100 пикселов, мы ищем фото, которое должно быть больше
+              var nodeNum = 0;
+              for(var i = 0; i < dom.length; i++) {
+                console.log(dom[i].attribs.width);
+                if (Number(dom[i].attribs.width) > maxWidth) {
+                    maxWidth = dom[i].width;
+                    nodeNum = i;
+                }
+              }
+            }
+            if (nodeNum == 0) {fail = true;}
+            
+            console.log(dom[nodeNum].attribs);
+          }
+        });
+        var parser = new htmlparser.Parser(handler);
+        parser.write(body);
+        parser.done();
+    
 
-        //request.get('https:' + imgSrc, function (error, response, body) {
-        request('https:' + imgSrc).pipe(fs.createWriteStream('doodle.jpg'));
-        /*
-        fs.readFile('app.js', function(err, data) {
-            if (err) throw err;
-            console.log(data);
-            
-            
-            var Canvas = require('canvas')
-            , Image = new Canvas.Image
-            , canvas = new Canvas(200, 200)
-            , ctx = canvas.getContext('2d');
 
-            Image.src = data;
-        
-            ctx.drawImage(Image, 0, 0, 50, 50);
-            
-            
-            res.send('OK');
-            //res.send('<img src="' + canvas.toDataURL() + '" />');
-          });  */
-      res.send('OK');
+
+        res.send("ok");
     }
   });
 });
