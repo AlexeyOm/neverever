5ccdf881cf5a8ccedc571d0b8cc4ec14906c2c8d
Alexey Omelchenko <alexey.omelchenko@gmail.com>
2016-10-24 19:59:35 +0000

    button caption reflects gender change, gender is send as GET param to openfile

--- public/javascripts/getpic.js
+++ public/javascripts/getpic.js
@@ -1,19 +1,27 @@
 $(document).ready(function () {
   $("#submitName").click(function () {
-    $.get(window.location.href + 'openfile?name=' + encodeURI($("#authorName").val()), function(data) {
+    $.get(window.location.href + 'openfile?name=' + encodeURI($("#authorName").val()) + '&gender=' + $("input[name=gender]:radio:checked").val(), function(data) {
       $("#resultImg").attr({src : data});
      
     });
   });
+  
   $("#authorName").keypress(function (event) {
     if(event.which == 13) {
       //event.preventDefault()
-      $.get(window.location.href + 'openfile?name=' + encodeURI($("#authorName").val()), function(data) {
+      $.get(window.location.href + 'openfile?name=' + encodeURI($("#authorName").val()) + '&gender=' + $("input[name=gender]:radio:checked").val(), function(data) {
         $("#resultImg").attr({src : data});
       });
     }
   });
+  
   $("#authorName").click(function (event) {
     $("#authorName").val("");
+  });
+  
+  $("input[name=gender]:radio").click(function() {
+    console.log(this);
+    if($(this).val() == 'f') {$("#submitName").val("Никогда я такого не говорила!");}
+    else {$("#submitName").val("Никогда я такого не говорил!");}
   });
 });
